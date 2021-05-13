e45a65ea91f372998c6e42aef14d1a65e26caf95
Petya Buchkova <petya@traffic-lab-macbook-petya.home>
2021-05-13 12:51:05 +0200

    Project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 /*
-*  Copyright (C) 1998-2021 by Northwoods Software Corporation. All Rights Reserved.
-*/
+ *  Copyright (C) 1998-2021 by Northwoods Software Corporation. All Rights Reserved.
+ */
 
-import * as go from 'gojs';
-import { produce } from 'immer';
-import * as React from 'react';
+import * as go from 'gojs'
+import { produce } from 'immer'
+import * as React from 'react'
 
-import { DiagramWrapper } from './components/DiagramWrapper';
-import { SelectionInspector } from './components/SelectionInspector';
+import { DiagramWrapper } from './components/DiagramWrapper'
+import { SelectionInspector } from './components/SelectionInspector'
 
-import './App.css';
+import './App.css'
 
 /**
  * Use a linkDataArray since we'll be using a GraphLinksModel,
@@ -17,70 +17,71 @@ import './App.css';
  * both are optional props in ReactDiagram.
  */
 interface AppState {
-  nodeDataArray: Array<go.ObjectData>;
-  linkDataArray: Array<go.ObjectData>;
-  modelData: go.ObjectData;
-  selectedData: go.ObjectData | null;
-  skipsDiagramUpdate: boolean;
+  nodeDataArray: Array<go.ObjectData>
+  linkDataArray: Array<go.ObjectData>
+  modelData: go.ObjectData
+  selectedData: go.ObjectData | null
+  skipsDiagramUpdate: boolean
 }
 
 class App extends React.Component<{}, AppState> {
   // Maps to store key -> arr index for quick lookups
-  private mapNodeKeyIdx: Map<go.Key, number>;
-  private mapLinkKeyIdx: Map<go.Key, number>;
+  private mapNodeKeyIdx: Map<go.Key, number>
+  private mapLinkKeyIdx: Map<go.Key, number>
 
   constructor(props: object) {
-    super(props);
+    super(props)
     this.state = {
       nodeDataArray: [
         { key: 0, text: 'Alpha', color: 'lightblue', loc: '0 0' },
         { key: 1, text: 'Beta', color: 'orange', loc: '150 0' },
         { key: 2, text: 'Gamma', color: 'lightgreen', loc: '0 150' },
-        { key: 3, text: 'Delta', color: 'pink', loc: '150 150' }
+        { key: 3, text: 'Delta', color: 'pink', loc: '150 150' },
       ],
       linkDataArray: [
         { key: -1, from: 0, to: 1 },
         { key: -2, from: 0, to: 2 },
         { key: -3, from: 1, to: 1 },
         { key: -4, from: 2, to: 3 },
-        { key: -5, from: 3, to: 0 }
+        { key: -5, from: 3, to: 0 },
       ],
       modelData: {
-        canRelink: true
+        canRelink: true,
       },
       selectedData: null,
-      skipsDiagramUpdate: false
-    };
+      skipsDiagramUpdate: false,
+    }
     // init maps
-    this.mapNodeKeyIdx = new Map<go.Key, number>();
-    this.mapLinkKeyIdx = new Map<go.Key, number>();
-    this.refreshNodeIndex(this.state.nodeDataArray);
-    this.refreshLinkIndex(this.state.linkDataArray);
+    // TODO: Add a function to fetch data from files
+    this.mapNodeKeyIdx = new Map<go.Key, number>()
+    this.mapLinkKeyIdx = new Map<go.Key, number>()
+    this.refreshNodeIndex(this.state.nodeDataArray)
+    this.refreshLinkIndex(this.state.linkDataArray)
     // bind handler methods
-    this.handleDiagramEvent = this.handleDiagramEvent.bind(this);
-    this.handleModelChange = this.handleModelChange.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleRelinkChange = this.handleRelinkChange.bind(this);
+    this.handleDiagramEvent = this.handleDiagramEvent.bind(this)
+    this.handleModelChange = this.handleModelChange.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleRelinkChange = this.handleRelinkChange.bind(this)
   }
 
   /**
    * Update map of node keys to their index in the array.
    */
   private refreshNodeIndex(nodeArr: Array<go.ObjectData>) {
-    this.mapNodeKeyIdx.clear();
+    this.mapNodeKeyIdx.clear()
     nodeArr.forEach((n: go.ObjectData, idx: number) => {
-      this.mapNodeKeyIdx.set(n.key, idx);
-    });
+      this.mapNodeKeyIdx.set(n.key, idx)
+    })
   }
 
   /**
    * Update map of link keys to their index in the array.
    */
   private refreshLinkIndex(linkArr: Array<go.ObjectData>) {
-    this.mapLinkKeyIdx.clear();
+    this.mapLinkKeyIdx.clear()
     linkArr.forEach((l: go.ObjectData, idx: number) => {
-      this.mapLinkKeyIdx.set(l.key, idx);
-    });
+      this.mapLinkKeyIdx.set(l.key, idx)
+    })
   }
 
   /**
@@ -89,34 +90,35 @@ class App extends React.Component<{}, AppState> {
    * @param e a GoJS DiagramEvent
    */
   public handleDiagramEvent(e: go.DiagramEvent) {
-    const name = e.name;
+    const name = e.name
     switch (name) {
       case 'ChangedSelection': {
-        const sel = e.subject.first();
+        const sel = e.subject.first()
         this.setState(
           produce((draft: AppState) => {
             if (sel) {
               if (sel instanceof go.Node) {
-                const idx = this.mapNodeKeyIdx.get(sel.key);
+                const idx = this.mapNodeKeyIdx.get(sel.key)
                 if (idx !== undefined && idx >= 0) {
-                  const nd = draft.nodeDataArray[idx];
-                  draft.selectedData = nd;
+                  const nd = draft.nodeDataArray[idx]
+                  draft.selectedData = nd
                 }
               } else if (sel instanceof go.Link) {
-                const idx = this.mapLinkKeyIdx.get(sel.key);
+                const idx = this.mapLinkKeyIdx.get(sel.key)
                 if (idx !== undefined && idx >= 0) {
-                  const ld = draft.linkDataArray[idx];
-                  draft.selectedData = ld;
+                  const ld = draft.linkDataArray[idx]
+                  draft.selectedData = ld
                 }
               }
             } else {
-              draft.selectedData = null;
+              draft.selectedData = null
             }
           })
-        );
-        break;
+        )
+        break
       }
-      default: break;
+      default:
+        break
     }
   }
 
@@ -126,93 +128,95 @@ class App extends React.Component<{}, AppState> {
    * @param obj a JSON-formatted string
    */
   public handleModelChange(obj: go.IncrementalData) {
-    const insertedNodeKeys = obj.insertedNodeKeys;
-    const modifiedNodeData = obj.modifiedNodeData;
-    const removedNodeKeys = obj.removedNodeKeys;
-    const insertedLinkKeys = obj.insertedLinkKeys;
-    const modifiedLinkData = obj.modifiedLinkData;
-    const removedLinkKeys = obj.removedLinkKeys;
-    const modifiedModelData = obj.modelData;
+    const insertedNodeKeys = obj.insertedNodeKeys
+    const modifiedNodeData = obj.modifiedNodeData
+    const removedNodeKeys = obj.removedNodeKeys
+    const insertedLinkKeys = obj.insertedLinkKeys
+    const modifiedLinkData = obj.modifiedLinkData
+    const removedLinkKeys = obj.removedLinkKeys
+    const modifiedModelData = obj.modelData
 
     // maintain maps of modified data so insertions don't need slow lookups
-    const modifiedNodeMap = new Map<go.Key, go.ObjectData>();
-    const modifiedLinkMap = new Map<go.Key, go.ObjectData>();
+    const modifiedNodeMap = new Map<go.Key, go.ObjectData>()
+    const modifiedLinkMap = new Map<go.Key, go.ObjectData>()
     this.setState(
       produce((draft: AppState) => {
-        let narr = draft.nodeDataArray;
+        let narr = draft.nodeDataArray
         if (modifiedNodeData) {
           modifiedNodeData.forEach((nd: go.ObjectData) => {
-            modifiedNodeMap.set(nd.key, nd);
-            const idx = this.mapNodeKeyIdx.get(nd.key);
+            modifiedNodeMap.set(nd.key, nd)
+            const idx = this.mapNodeKeyIdx.get(nd.key)
             if (idx !== undefined && idx >= 0) {
-              narr[idx] = nd;
+              narr[idx] = nd
               if (draft.selectedData && draft.selectedData.key === nd.key) {
-                draft.selectedData = nd;
+                draft.selectedData = nd
               }
             }
-          });
+          })
         }
         if (insertedNodeKeys) {
           insertedNodeKeys.forEach((key: go.Key) => {
-            const nd = modifiedNodeMap.get(key);
-            const idx = this.mapNodeKeyIdx.get(key);
-            if (nd && idx === undefined) {  // nodes won't be added if they already exist
-              this.mapNodeKeyIdx.set(nd.key, narr.length);
-              narr.push(nd);
+            const nd = modifiedNodeMap.get(key)
+            const idx = this.mapNodeKeyIdx.get(key)
+            if (nd && idx === undefined) {
+              // nodes won't be added if they already exist
+              this.mapNodeKeyIdx.set(nd.key, narr.length)
+              narr.push(nd)
             }
-          });
+          })
         }
         if (removedNodeKeys) {
           narr = narr.filter((nd: go.ObjectData) => {
             if (removedNodeKeys.includes(nd.key)) {
-              return false;
+              return false
             }
-            return true;
-          });
-          draft.nodeDataArray = narr;
-          this.refreshNodeIndex(narr);
+            return true
+          })
+          draft.nodeDataArray = narr
+          this.refreshNodeIndex(narr)
         }
 
-        let larr = draft.linkDataArray;
+        let larr = draft.linkDataArray
         if (modifiedLinkData) {
           modifiedLinkData.forEach((ld: go.ObjectData) => {
-            modifiedLinkMap.set(ld.key, ld);
-            const idx = this.mapLinkKeyIdx.get(ld.key);
+            modifiedLinkMap.set(ld.key, ld)
+            const idx = this.mapLinkKeyIdx.get(ld.key)
             if (idx !== undefined && idx >= 0) {
-              larr[idx] = ld;
+              larr[idx] = ld
               if (draft.selectedData && draft.selectedData.key === ld.key) {
-                draft.selectedData = ld;
+                draft.selectedData = ld
               }
             }
-          });
+          })
         }
         if (insertedLinkKeys) {
           insertedLinkKeys.forEach((key: go.Key) => {
-            const ld = modifiedLinkMap.get(key);
-            const idx = this.mapLinkKeyIdx.get(key);
-            if (ld && idx === undefined) {  // links won't be added if they already exist
-              this.mapLinkKeyIdx.set(ld.key, larr.length);
-              larr.push(ld);
+            const ld = modifiedLinkMap.get(key)
+            const idx = this.mapLinkKeyIdx.get(key)
+            if (ld && idx === undefined) {
+              // links won't be added if they already exist
+              this.mapLinkKeyIdx.set(ld.key, larr.length)
+              larr.push(ld)
             }
-          });
+          })
         }
         if (removedLinkKeys) {
           larr = larr.filter((ld: go.ObjectData) => {
             if (removedLinkKeys.includes(ld.key)) {
-              return false;
+              return false
             }
-            return true;
-          });
-          draft.linkDataArray = larr;
-          this.refreshLinkIndex(larr);
+            return true
+          })
+          draft.linkDataArray = larr
+          this.refreshLinkIndex(larr)
         }
         // handle model data changes, for now just replacing with the supplied object
         if (modifiedModelData) {
-          draft.modelData = modifiedModelData;
+          draft.modelData = modifiedModelData
         }
-        draft.skipsDiagramUpdate = true;  // the GoJS model already knows about these updates
+        draft.skipsDiagramUpdate = true // the GoJS model already knows about these updates
       })
-    );
+    )
   }
 
   /**
@@ -224,26 +228,27 @@ class App extends React.Component<{}, AppState> {
   public handleInputChange(path: string, value: string, isBlur: boolean) {
     this.setState(
       produce((draft: AppState) => {
-        const data = draft.selectedData as go.ObjectData;  // only reached if selectedData isn't null
-        data[path] = value;
+        const data = draft.selectedData as go.ObjectData // only reached if selectedData isn't null
+        data[path] = value
         if (isBlur) {
-          const key = data.key;
-          if (key < 0) {  // negative keys are links
-            const idx = this.mapLinkKeyIdx.get(key);
+          const key = data.key
+          if (key < 0) {
+            // negative keys are links
+            const idx = this.mapLinkKeyIdx.get(key)
             if (idx !== undefined && idx >= 0) {
-              draft.linkDataArray[idx] = data;
-              draft.skipsDiagramUpdate = false;
+              draft.linkDataArray[idx] = data
+              draft.skipsDiagramUpdate = false
             }
           } else {
-            const idx = this.mapNodeKeyIdx.get(key);
+            const idx = this.mapNodeKeyIdx.get(key)
             if (idx !== undefined && idx >= 0) {
-              draft.nodeDataArray[idx] = data;
-              draft.skipsDiagramUpdate = false;
+              draft.nodeDataArray[idx] = data
+              draft.skipsDiagramUpdate = false
             }
           }
         }
       })
-    );
+    )
   }
 
   /**
@@ -251,31 +256,46 @@ class App extends React.Component<{}, AppState> {
    * @param e a change event from the checkbox
    */
   public handleRelinkChange(e: any) {
-    const target = e.target;
-    const value = target.checked;
-    this.setState({ modelData: { canRelink: value }, skipsDiagramUpdate: false });
+    const target = e.target
+    const value = target.checked
+    this.setState({
+      modelData: { canRelink: value },
+      skipsDiagramUpdate: false,
+    })
   }
 
   public render() {
-    const selectedData = this.state.selectedData;
-    let inspector;
+    const selectedData = this.state.selectedData
+    let inspector
     if (selectedData !== null) {
-      inspector = <SelectionInspector
-                    selectedData={this.state.selectedData}
-                    onInputChange={this.handleInputChange}
-                  />;
+      inspector = (
+        <SelectionInspector
+          selectedData={this.state.selectedData}
+          onInputChange={this.handleInputChange}
+        />
+      )
     }
 
     return (
       <div>
         <p>
-          Try moving around nodes, editing text, relinking, undoing (Ctrl-Z), etc. within the diagram
-          and you'll notice the changes are reflected in the inspector area. You'll also notice that changes
-          made in the inspector are reflected in the diagram. If you use the React dev tools,
-          you can inspect the React state and see it updated as changes happen.
+          Try moving around nodes, editing text, relinking, undoing (Ctrl-Z),
+          etc. within the diagram and you'll notice the changes are reflected in
+          the inspector area. You'll also notice that changes made in the
+          inspector are reflected in the diagram. If you use the React dev
+          tools, you can inspect the React state and see it updated as changes
+          happen.
         </p>
         <p>
-          Check out the <a href='https://gojs.net/latest/intro/react.html' target='_blank' rel='noopener noreferrer'>Intro page on using GoJS with React</a> for more information.
+          Check out the{' '}
+          <a
+            href="https://gojs.net/latest/intro/react.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Intro page on using GoJS with React
+          </a>{' '}
+          for more information.
         </p>
         <DiagramWrapper
           nodeDataArray={this.state.nodeDataArray}
@@ -288,15 +308,16 @@ class App extends React.Component<{}, AppState> {
         <label>
           Allow Relinking?
           <input
-            type='checkbox'
-            id='relink'
+            type="checkbox"
+            id="relink"
             checked={this.state.modelData.canRelink}
-            onChange={this.handleRelinkChange} />
+            onChange={this.handleRelinkChange}
+          />
         </label>
         {inspector}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
