dae7422b74b65a79f998f1e255bc28a66601c63b
Petya Buchkova <petya.buchkova@gmail.com>
2021-05-16 12:48:20 +0200

    Styled page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,9 @@ const express = require('express')
 const router = express.Router()
 const fileController = require('../controllers/fileController')
 
-// Follows HTTP verbs
 router.get('/', async (req, res) => {
   const diagramData = await fileController.getModuleDiagramData()
-
-  // res.sendFile('index.html', { root: './src/views' })
   res.render('index', {
-    email: 'this is a passed variable',
     data: JSON.stringify(diagramData),
   })
 })
